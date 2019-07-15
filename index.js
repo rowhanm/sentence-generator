e67af7f0de58c86b91033ac071c83d8b925640bc
Rohan M <rohanmahadev7@gmail.com>
2019-07-15 14:07:27 +0200

    cleanup

--- index.js
+++
@@ -1,5 +0,0 @@
-const request = require('request');
-const bookname = "harry potter chamber of secrets"
-request('https://www.googleapis.com/books/v1/volumes?q=' + bookname+'&orderBy=relevance&langRestrict=en', function (error, response, body) {
-  console.log(JSON.parse(body).items[0].volumeInfo);
-});
