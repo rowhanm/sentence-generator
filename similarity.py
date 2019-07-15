2adbc8754acd280892fe224a17db571b599fac06
Rohan M <rohanmahadev7@gmail.com>
2019-07-15 14:35:18 +0200

    added model

--- similarity.py
+++ similarity.py
@@ -1,10 +1,11 @@
 import spacy
-import time
+import json
 
 import requests
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+nlp = spacy.load('en_core_web_lg')
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
@@ -20,11 +21,45 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        fields = json.loads(post_data.decode('utf-8'))
+        bookName = fields["bookName"]
+        author = fields["author"]
+        print(bookName, author)        
+        response = requests.get(
+        'https://www.googleapis.com/books/v1/volumes?q=' + bookName + " " + author + '&orderBy=relevance&langRestrict=en'
+        )
 
+        flag = 0
+        json_response = response.json()
+        if len(json_response['items']) < 0:
+                category = "Default"
+                flag = 1
+        else:               
+                category = json_response['items'][0]['volumeInfo']['categories'][0]
+
+        categories = ["Arts and photography", "Biographies and memoirs", "Business and investing", "Children's books", "Cookbooks, food and wine", "History", "Literature and Fiction", "Mystery and suspense", "Romance", "Sci-Fi and Fantasy", "Teens and young adults"]
+
+        if flag == 1:
+                print("Default")
+                self._set_response()
+                self.wfile.write("Default!")
+        source = nlp(category) 
+        maxval = 0.0
+        maxtar = ""
+
+        for i in categories:
+                target = nlp(i)
+                calc = target.similarity(source)
+                if calc > maxval:
+                        maxval = calc
+                        maxtar = i
+        result = {
+                "category": maxtar
+        }
+
+        print(str(maxval) + ": " + maxtar)
         self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write(maxtar.encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
@@ -48,39 +83,4 @@ if __name__ == '__main__':
 
 
 
-# bookName = "homo deus"
-# author = "harari"
 
-# response = requests.get(
-#     'https://www.googleapis.com/books/v1/volumes?q=' + bookName + " " + author + '&orderBy=relevance&langRestrict=en'
-# )
-
-# flag = 0
-# json_response = response.json()
-# if len(json_response['items']) < 0:
-#         category = "Default"
-#         flag = 1
-# else:               
-#         category = json_response['items'][0]['volumeInfo']['categories'][0]
-
-# start_time = time.time()
-# nlp = spacy.load('en_core_web_lg')
-
-# categories = ["Arts and photography", "Biographies and memoirs", "Business and investing", "Children's books", "Cookbooks, food and wine", "History", "Literature and Fiction", "Mystery and suspense", "Romance", "Sci-Fi and Fantasy", "Teens and young adults"]
-
-# if flag == 1:
-#         print("Default")
-#         exit()
-# source = nlp(category) 
-# maxval = 0.0
-# maxtar = ""
-
-# for i in categories:
-#     target = nlp(i)
-#     calc = target.similarity(source)
-#     if calc > maxval:
-#         maxval = calc
-#         maxtar = i
-
-# print(str(maxval) + ": " + maxtar)
-# print("--- %s seconds ---" % (time.time() - start_time))
